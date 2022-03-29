ee55410377c4c1301ddf7b3fcbca3f4c4a706c9c
MH102 <aozhen01@gmail.com>
2022-03-29 14:51:49 -0600

    file reading and random

--- Planificador.c
+++ Planificador.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int testReadRand();
 
-int main(){
-    
+int main()
+{
+    testReadRand();
+    return 0;
+}
 
-    printf("Hello world");
+// Placeholder function for file reading and random gen tests
+int testReadRand()
+{
+    int burst, priority, randInt;
 
+    // SEED INIT
+    srand(time(NULL));
+
+    FILE *reader = fopen("test.txt", "r");
+
+    // If file does not exist
+    if (reader == NULL)
+    {
+        return 1;
+    }
+
+    // checks file for each line
+    while (fscanf(reader, "%d %d", &burst, &priority) == 2)
+    {
+        // Simple random (1-10)
+        randInt = rand() % 10;
+
+        // Each time should have a certain amount of sleep
+        // Here is where you send the data through the sockets on individual threads
+        printf("BURST=%d, PRIORITY=%d, RAND=%d\n", burst, priority, randInt);
+    }
+    fclose(reader);
     return 0;
 }
