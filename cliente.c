6b660132e1f1a81efd53606cad7d103932768ac1
valevqb03 <44345368+valevqb@users.noreply.github.com>
2022-04-10 14:41:48 -0400

    Cliente con quit

--- cliente.c
+++ cliente.c
@@ -6,6 +6,10 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define PROCESS_MAX 20
 #define COLS 2
@@ -17,14 +21,46 @@ struct Message
     char *message;
 };
 
+struct Stop
+{
+	int stopC;
+};
+
+struct Stop stopServer;
+
+int getChar(){
+	int c;
+    int oc = '\0';
+    struct termios staryTermios, novyTermios;
+    int oflags, nflags;
+
+    novyTermios = staryTermios;
+    novyTermios.c_lflag &= ~(ICANON);
+
+    oflags = fcntl(STDIN_FILENO, F_GETFL);
+
+    nflags = oflags;
+    nflags |= O_NONBLOCK;
+	fcntl(STDIN_FILENO, F_SETFL, nflags);
+	
+	if (c = getchar() == 'q'){
+		stopServer.stopC = 1;
+		return 1;
+	}
+	return 0;
+}
+
 // Concat char to string
 char *ConcatCharToCharArray(char *Str, char Chr)
 {
     int len = strlen(Str);
     char *StrResult = malloc(len + 2);
-    strcpy(StrResult, Str);
-    StrResult[len] = Chr;
-    StrResult[len + 1] = '\0';
+	while (stopServer.stopC != 1 & getChar() != 1){
+		strcpy(StrResult, Str);
+		StrResult[len] = Chr;
+		StrResult[len + 1] = '\0';
+		break;
+	}
     return StrResult;
 }
 
@@ -32,82 +68,96 @@ char *ConcatCharToCharArray(char *Str, char Chr)
 char *numberToString(int number)
 {
     char *string = (char *)malloc(sizeof(char));
-    sprintf(string, "%d", number);
+	while (stopServer.stopC != 1 & getChar() != 1){
+		sprintf(string, "%d", number);
+		break;
+	}
     return string;
 }
 
 // Send to server and wait reply
 void *sendProcessSocket(void *msg)
 {
-    struct Message *my_msg = (struct Message *)msg;
-    int valread;
-    char buffer[2000] = {};
-    printf("Message: %s\n", my_msg->message);       // Solo para pruebas(ELIMINAR LUEGO)
-    printf("Socket cliente: %d\n", my_msg->socket); // Solo para pruebas(ELIMINAR LUEGO)
-    send(my_msg->socket, my_msg->message, strlen(my_msg->message), 0);
-    valread = read(my_msg->socket, buffer, 2000);
-    puts(buffer);
+	while (stopServer.stopC != 1 & getChar() != 1){
+		struct Message *my_msg = (struct Message *)msg;
+		int valread;
+		char buffer[2000] = {};
+		printf("Message: %s\n", my_msg->message);       // Solo para pruebas(ELIMINAR LUEGO)
+		printf("Socket cliente: %d\n", my_msg->socket); // Solo para pruebas(ELIMINAR LUEGO)
+		send(my_msg->socket, my_msg->message, strlen(my_msg->message), 0);
+		valread = read(my_msg->socket, buffer, 2000);
+		puts(buffer);
+		break;
+	}
 }
 
 // Make thread by send data to server
 void sendProcess(int burst, int priority, int socket)
 {
-    sleep(2);
-    char *msgOut = numberToString(burst);
-    char *numString = numberToString(priority);
-    msgOut = ConcatCharToCharArray(msgOut, ',');
-    msgOut = strcat(msgOut, numString);
+	while (stopServer.stopC != 1 & getChar() != 1){
+		sleep(2);
+		char *msgOut = numberToString(burst);
+		char *numString = numberToString(priority);
+		msgOut = ConcatCharToCharArray(msgOut, ',');
+		msgOut = strcat(msgOut, numString);
 
-    struct Message *msg = malloc(sizeof(struct Message));
-    msg->message = msgOut;
-    msg->socket = socket;
+		struct Message *msg = malloc(sizeof(struct Message));
+		msg->message = msgOut;
+		msg->socket = socket;
 
-    // Created thread and function call
-    pthread_t thrd;
-    pthread_create(&thrd, NULL, sendProcessSocket, (void *)msg);
-    pthread_join(thrd, NULL);
+		// Created thread and function call
+		pthread_t thrd;
+		pthread_create(&thrd, NULL, sendProcessSocket, (void *)msg);
+		pthread_join(thrd, NULL);
+		break;
+	}
 }
 
 // Make random data by socket to server
 void randProcess(int socket, int time)
 {
 
-    int burst = rand() % (5 + 1 - 1) + 1;
-    int priority = rand() % (5 + 1 - 1) + 1;
-    sendProcess(burst, priority, socket);
-    sleep(time);
+	while (stopServer.stopC != 1 & getChar() != 1){
+		int burst = rand() % (5 + 1 - 1) + 1;
+		int priority = rand() % (5 + 1 - 1) + 1;
+		sendProcess(burst, priority, socket);
+		sleep(time);
+		break;
+	}
 }
 
 // Placeholder function for file reading and random gen tests
 int testReadRand(int socket, char *txtName)
 {
 
-    srand(time(NULL));
+	while (stopServer.stopC != 1 & getChar() != 1){
+		srand(time(NULL));
 
-    int burst, priority, time;
+		int burst, priority, time;
 
-    FILE *reader = fopen(txtName, "r");
+		FILE *reader = fopen(txtName, "r");
 
-    // If file does not exist
-    if (reader == NULL)
-    {
-        return 0;
-    }
+		// If file does not exist
+		if (reader == NULL)
+		{
+			return 0;
+		}
 
-    // checks file for each line
-    while (fscanf(reader, "%d %d", &burst, &priority) == 2)
-    {
-        time = rand() % (8 + 1 - 3) + 3;
-        // Send data by socket  to server
-        sendProcess(burst, priority, socket);
-        sleep(time);
+		// checks file for each line
+		while (fscanf(reader, "%d %d", &burst, &priority) == 2)
+		{
+			time = rand() % (8 + 1 - 3) + 3;
+			// Send data by socket  to server
+			sendProcess(burst, priority, socket);
+			sleep(time);
 
-        // Each time should have a certain amount of sleep
-        // Here is where you send the data through the sockets on individual threads
-    }
-    fclose(reader);
-
-    return 1;
+			// Each time should have a certain amount of sleep
+			// Here is where you send the data through the sockets on individual threads
+		}
+		fclose(reader);
+		break;
+	}
+	return 1;
 }
 
 void *autoCPU(int socket)
@@ -116,8 +166,7 @@ void *autoCPU(int socket)
 
     printf("Ingrese el tiempo de espera entre procesos \n");
     scanf("%d", &time);
-    while (1)
-    {
+    while (stopServer.stopC != 1 & getChar() != 1){
         randProcess(socket, time);
     }
 }
